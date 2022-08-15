db9ac4aa27ce7e77a0b66653c7fed87d76945bdc
wangminxiang <wangminxiang@qq.com>
2022-08-15 08:46:52 +0800

    11

--- src/index.js
+++ src/index.js
@@ -30,124 +30,124 @@ if(!COOKIE) {
   junJin().then(() => {});
 }
 
-// let juejinUid = '';
-//
-// if (!(COOKIE && TOKEN)) {
-//   message('获取不到游戏必须得COOKIE和TOKEN，请检查设置')
-// } else {
-//   let gameId = ''; // 发指令必须得gameId
-//   let deep = 0;
-//   let todayDiamond = 0;
-//   let todayLimitDiamond = 0;
-//   async function getInfo() {
-//     const time = new Date().getTime();
-//     console.log(todayDiamond, todayLimitDiamond);
-//     const userInfo = await miningApi.getUser();
-//     juejinUid = userInfo.user_id;
-//
-//     const resInfo = await miningApi.getInfo(juejinUid, time);
-//     deep = resInfo.gameInfo ? resInfo.gameInfo.deep : 0;
-//     gameId = resInfo.gameInfo ? resInfo.gameInfo.gameId : 0;
-//     todayDiamond = resInfo.userInfo.todayDiamond || 0;
-//     todayLimitDiamond = resInfo.userInfo.todayLimitDiamond;
-//     return Promise.resolve(resInfo);
-//   }
-//   getInfo().then(() => {
-//     if (todayDiamond < todayLimitDiamond) {
-//       playGame().then(() => {});
-//     }
-//   });
-//
-//   // 暂停，避免快速请求以及频繁请求
-//   async function sleep(delay) {
-//     return new Promise(((resolve) => setTimeout(resolve, delay)));
-//   }
-//   /**
-//    * 循环游戏
-//    */
-//   async function playGame() {
-//     try {
-//       // 开始
-//       const startTime = new Date().getTime();
-//       const startParams = {
-//         roleId: 3,
-//       };
-//       const startData = await miningApi.start(startParams, juejinUid, startTime);
-//       await sleep(3000);
-//       console.log('startData', startData);
-//       gameId = startData.gameId;
-//       // 发起指令
-//       const commandTime = +new Date().getTime();
-//       const commandParams = {
-//         command: firstData.command,
-//       };
-//       const xGameId = getXGameId(gameId);
-//       const commandData = await miningApi.command(commandParams, juejinUid, commandTime, xGameId);
-//       deep = commandData.curPos.y;
-//       await sleep(3000);
-//       console.log('commandData', commandData);
-//       // 结束
-//       const overTime = +new Date().getTime();
-//       const overParams = {
-//         isButton: 1,
-//       };
-//       const overData = await miningApi.over(overParams, juejinUid, overTime);
-//       await sleep(3000);
-//       console.log('overData', overData);
-//       deep = overData.deep;
-//       // 更换地图
-//       const mapTime = +new Date().getTime();
-//       if (deep < 500) {
-//         await sleep(3000);
-//         await miningApi.freshMap({}, juejinUid, mapTime);
-//       }
-//       await sleep(3000);
-//       await getInfo().then((res) => {
-//         if (todayDiamond < todayLimitDiamond) {
-//           playGame()
-//         } else {
-//           message(`今日限制矿石${res.userInfo.todayLimitDiamond},已获取矿石${res.userInfo.todayDiamond}`)
-//         }
-//       });
-//     } catch(e) {
-//       console.log(e);
-//       await sleep(3000);
-//       // 结束
-//       const overTime = +new Date().getTime();
-//       const overParams = {
-//         isButton: 1,
-//       };
-//       await miningApi.over(overParams, juejinUid, overTime);
-//       await sleep(3000);
-//       await getInfo().then((res) => {
-//         if (todayDiamond < todayLimitDiamond) {
-//           playGame()
-//         } else {
-//           message(`今日限制矿石${res.userInfo.todayLimitDiamond},已获取矿石${res.userInfo.todayDiamond}`)
-//         }
-//       });
-//     }
-//   }
-//   function getXGameId(id) {
-//     const time = +new Date().getTime();
-//     return jwt.sign(
-//       {
-//         gameId: id,
-//         time: time,
-//         // eslint-disable-next-line max-len
-//       },
-//       "-----BEGIN EC PARAMETERS-----\nBggqhkjOPQMBBw==\n-----END EC PARAMETERS-----\n-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIDB7KMVQd+eeKt7AwDMMUaT7DE3Sl0Mto3LEojnEkRiAoAoGCCqGSM49\nAwEHoUQDQgAEEkViJDU8lYJUenS6IxPlvFJtUCDNF0c/F/cX07KCweC4Q/nOKsoU\nnYJsb4O8lMqNXaI1j16OmXk9CkcQQXbzfg==\n-----END EC PRIVATE KEY-----\n",
-//       {
-//         algorithm: "ES256",
-//         expiresIn: 2592e3,
-//         header: {
-//           alg: "ES256",
-//           typ: "JWT",
-//         },
-//       }
-//     );
-//   }
-//}
+let juejinUid = '';
+
+if (!(COOKIE && TOKEN)) {
+  message('获取不到游戏必须得COOKIE和TOKEN，请检查设置')
+} else {
+  let gameId = ''; // 发指令必须得gameId
+  let deep = 0;
+  let todayDiamond = 0;
+  let todayLimitDiamond = 0;
+  async function getInfo() {
+    const time = new Date().getTime();
+    console.log(todayDiamond, todayLimitDiamond);
+    const userInfo = await miningApi.getUser();
+    juejinUid = userInfo.user_id;
+
+    const resInfo = await miningApi.getInfo(juejinUid, time);
+    deep = resInfo.gameInfo ? resInfo.gameInfo.deep : 0;
+    gameId = resInfo.gameInfo ? resInfo.gameInfo.gameId : 0;
+    todayDiamond = resInfo.userInfo.todayDiamond || 0;
+    todayLimitDiamond = resInfo.userInfo.todayLimitDiamond;
+    return Promise.resolve(resInfo);
+  }
+  getInfo().then(() => {
+    if (todayDiamond < todayLimitDiamond) {
+      playGame().then(() => {});
+    }
+  });
+
+  // 暂停，避免快速请求以及频繁请求
+  async function sleep(delay) {
+    return new Promise(((resolve) => setTimeout(resolve, delay)));
+  }
+  /**
+   * 循环游戏
+   */
+  async function playGame() {
+    try {
+      // 开始
+      const startTime = new Date().getTime();
+      const startParams = {
+        roleId: 3,
+      };
+      const startData = await miningApi.start(startParams, juejinUid, startTime);
+      await sleep(3000);
+      console.log('startData', startData);
+      gameId = startData.gameId;
+      // 发起指令
+      const commandTime = +new Date().getTime();
+      const commandParams = {
+        command: firstData.command,
+      };
+      const xGameId = getXGameId(gameId);
+      const commandData = await miningApi.command(commandParams, juejinUid, commandTime, xGameId);
+      deep = commandData.curPos.y;
+      await sleep(3000);
+      console.log('commandData', commandData);
+      // 结束
+      const overTime = +new Date().getTime();
+      const overParams = {
+        isButton: 1,
+      };
+      const overData = await miningApi.over(overParams, juejinUid, overTime);
+      await sleep(3000);
+      console.log('overData', overData);
+      deep = overData.deep;
+      // 更换地图
+      const mapTime = +new Date().getTime();
+      if (deep < 500) {
+        await sleep(3000);
+        await miningApi.freshMap({}, juejinUid, mapTime);
+      }
+      await sleep(3000);
+      await getInfo().then((res) => {
+        if (todayDiamond < todayLimitDiamond) {
+          playGame()
+        } else {
+          message(`今日限制矿石${res.userInfo.todayLimitDiamond},已获取矿石${res.userInfo.todayDiamond}`)
+        }
+      });
+    } catch(e) {
+      console.log(e);
+      await sleep(3000);
+      // 结束
+      const overTime = +new Date().getTime();
+      const overParams = {
+        isButton: 1,
+      };
+      await miningApi.over(overParams, juejinUid, overTime);
+      await sleep(3000);
+      await getInfo().then((res) => {
+        if (todayDiamond < todayLimitDiamond) {
+          playGame()
+        } else {
+          message(`今日限制矿石${res.userInfo.todayLimitDiamond},已获取矿石${res.userInfo.todayDiamond}`)
+        }
+      });
+    }
+  }
+  function getXGameId(id) {
+    const time = +new Date().getTime();
+    return jwt.sign(
+      {
+        gameId: id,
+        time: time,
+        // eslint-disable-next-line max-len
+      },
+      "-----BEGIN EC PARAMETERS-----\nBggqhkjOPQMBBw==\n-----END EC PARAMETERS-----\n-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIDB7KMVQd+eeKt7AwDMMUaT7DE3Sl0Mto3LEojnEkRiAoAoGCCqGSM49\nAwEHoUQDQgAEEkViJDU8lYJUenS6IxPlvFJtUCDNF0c/F/cX07KCweC4Q/nOKsoU\nnYJsb4O8lMqNXaI1j16OmXk9CkcQQXbzfg==\n-----END EC PRIVATE KEY-----\n",
+      {
+        algorithm: "ES256",
+        expiresIn: 2592e3,
+        header: {
+          alg: "ES256",
+          typ: "JWT",
+        },
+      }
+    );
+  }
+}
 
 
 
